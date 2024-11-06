d0a14370623d0f65a87f1584516f9cc3f89a744f
XavierGarc <xavgarrov@alu.edu.gva.es>
2024-11-06 13:42:15 +0100

    d

--- ejercicios_listas/lista_ord_alfabetico.py
+++ ejercicios_listas/lista_ord_alfabetico.py
@@ -1,8 +1,8 @@
 #En este ejercicio crearemos una lista que ordene los elementos por orden alfabetico
-elementos_lista = int(input('Cuantos elementos tendra nuestra lista? '))
-numero = 0
-lista = []
-while numero != elementos_lista:
-    lista = int(input('Introduce uno de los elementos de la lista: '))
-    elementos_lista = elementos_lista-1
-print(sorted(lista))
+cantidad=int(input('Digame cuantas palabras tendremos: '))
+nom=[]
+for i in range (1,cantidad+1):
+    nombre=input(f'Digame la palabra {i}: ')
+    nom.append(nombre)
+print(f'La lista creada es {nom}')
+print(f'La lista ordenada es : {sorted(nom)}')
