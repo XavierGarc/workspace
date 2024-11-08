ed6b7aaf690cc3d3264c267e8195edf6372b9768
XavierGarc <xavgarrov@alu.edu.gva.es>
2024-11-08 09:48:40 +0100

    h

--- ejercicios_diccionarios/diccionario_valor_min.py
+++ ejercicios_diccionarios/diccionario_valor_min.py
@@ -4,7 +4,7 @@ notas = {
  'Historia': 9
 }
 minimo = 10
-for i in notas:
-    if i.value() < minimo:
-        asignatura=i.key()
-print(f'El minimo es: {minimo}')
+for asignatura,nota in notas:
+    if nota < minimo:
+        asignatura_min=asignatura
+print(f'El minimo es: {asignatura_min}')
