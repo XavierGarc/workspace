68dcac0107f54183e174a4e03c2913653effbc57
XavierGarc <xavgarrov@alu.edu.gva.es>
2024-11-12 13:55:57 +0100

    ff

--- ejercicios_diccionarios/diccionario_valor_min.py
+++ ejercicios_diccionarios/diccionario_valor_min.py
@@ -3,8 +3,10 @@ notas = {
  'Física': 8,
  'Historia': 9
 }
-minimo = 10
-for asignatura,nota in notas:
-    if nota < minimo:
-        asignatura_min=asignatura
-print(f'El minimo es: {asignatura_min}')
+minimo=10
+for clave,valor in notas.items():
+    if valor < minimo:
+        minimo = valor
+        claveMenosNota = clave
+        
+print(f"La clave con el valor más bajo es {claveMenosNota} ")
