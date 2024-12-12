3dfdbd0889c41a651d8ef637273a0b7ae775c00d
XavierGarc <xavgarrov@alu.edu.gva.es>
2024-12-12 13:56:41 +0100

    h

--- Ejemplos_POO/Ejemplos_café/Coffee machine project/oop-coffee-machine-start/main2.py
+++ Ejemplos_POO/Ejemplos_café/Coffee machine project/oop-coffee-machine-start/main2.py
@@ -8,8 +8,8 @@ st.write("Enjoy your coffee today!")
 
 col1, col2 = st.columns([1, 1])
 with col1:
-    lat = st.number_input("Introducir Elección (latte/espresso/cappuccino):", value='espresso')
+    option = st.selectbox('Introducir Elección' ('latte'/'espresso'/'cappuccino'))
 with col2:
-    lon = st.number_input("Introducir Dinero:", value=0.0, format="%.6f", step=0.000001)
+    lon = st.number.input("Introducir Dinero:", value=0.0, format="%.6f", step=0.000001)
 
 fetch_button = st.button("Make Coffee")
